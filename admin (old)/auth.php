43c676b92c89b7d1b3d2f1af8049e2e1083bebde
SyedIhsan <syedihsan1998@gmail.com>
2026-02-14 10:05:49 +0800

    finish admin dashboard

--- admin (old)/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require __DIR__ . "/bootstrap.php";
-
-if (!is_admin()) {
-  redirect("login.php");
-}
--- admin (old)/secure/drive_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-const USERS_ROOT_FOLDER_ID = "1Ua4r4DU6Zy4mfqIthwL-OXhly6xHHo5a"; 
-?>
